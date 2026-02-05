2936a6e1bf60e470880672f5e9d8393dfdbad357
Somay's Laptop <somay052407@gmail.com>
2026-02-05 11:14:14 -0500

    this is a test

---
+++ Feb-05-2026.m
@@ -0,0 +1,19 @@
+clc, clearvars
+
+%% Example 1
+% Basic if statements
+x = input('Enter a number');
+if x > 0
+    disp('x is positive')
+end
+
+%% Example 2
+% If-else statements
+x = -3;
+if x >= 0
+    disp('x is nonnegative')
+else 
+    disp('x is negative')
+end
+
+
