a7d359b0ca563f0c53a91a1dc6f245c201439a7d
Somay's Laptop <somay052407@gmail.com>
2026-02-10 12:05:29 -0500

    Added: Examples 6-9 for Switches

--- Feb_10_2026.m
+++ Feb_10_2026.m
@@ -82,3 +82,61 @@ switch action
     otherwise
         disp('Invalid Option');
 end
+
+%% Example 6
+% Checking if a number is positive, negative, or 0
+
+number = 5;
+
+switch true
+    case number > 0
+        disp('The Number is Positive');
+    case number < 0
+        disp('The number is Negative');
+    otherwise 
+        disp('The number is Zero');
+end
+
+%% Example 7
+% Season based on the number of the month
+
+month = 2;
+
+switch true 
+    case (month > 2) && (month < 6)
+        disp('Spring');
+    case (month > 5) && (month < 9)
+        disp('Summer');
+    case (month > 8) && (month < 12)
+        disp('Autumn');
+    case (month > 11) || (month < 3)
+        disp('Winter');
+    otherwise
+        disp('Out of range');
+end
+
+%% Example 8
+% Temperature with if statements
+
+temperature = 25;
+
+if temperature < 10
+    disp('Cold');
+elseif (temperature >= 10 ) && (temperature < 25)
+    disp('Warm');
+elseif (temperature >= 25)
+    disp('Hot');
+end
+
+%% Example 9
+% Day of the week with switch
+
+day = 'Sun';
+switch day
+    case {'Sun', 'Sat'}
+        disp('It is the weekend');
+    case {'Mon', 'Tue', 'Wed', 'Thurs', 'Fri'}
+        disp('It is a weekday');
+    otherwise
+        disp('Invalid input');
+end
